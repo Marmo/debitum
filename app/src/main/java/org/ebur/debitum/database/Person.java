6293f6ada1ac96a54d2f885b1887b52d1f2b81ab
Marmo <Marmo@users.noreply.github.com>
2021-04-14 20:49:03 +0200

    Make Person Parcelable

--- app/src/main/java/org/ebur/debitum/database/Person.java
+++ app/src/main/java/org/ebur/debitum/database/Person.java
@@ -1,11 +1,14 @@
 package org.ebur.debitum.database;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "person")
-public class Person {
+public class Person implements Parcelable {
 
     public Person(String name) {
         this.name = name;
@@ -17,6 +20,35 @@ public class Person {
 
     public boolean equals(Person p) {
         return this.name.equals(p.name) && (this.idPerson == p.idPerson);
+    }
+
+    // -------------------------
+    // Parcelable implementation
+    // -------------------------
+
+    public int describeContents() {
+        return 0;
+    }
+
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeInt(idPerson);
+        out.writeString(name);
+    }
+
+    public static final Parcelable.Creator<Person> CREATOR
+            = new Parcelable.Creator<Person>() {
+        public Person createFromParcel(Parcel in) {
+            return new Person(in);
+        }
+
+        public Person[] newArray(int size) {
+            return new Person[size];
+        }
+    };
+
+    private Person(Parcel in) {
+        idPerson = in.readInt();
+        name = in.readString();
     }
 
 }
