7b1cee62275485b005581e6f0b1f42fa7c3713fb
Marmo <Marmo@users.noreply.github.com>
2021-04-18 07:23:03 +0200

    hide FAB on edit screens

--- app/src/main/java/org/ebur/debitum/ui/MainActivity.java
+++ app/src/main/java/org/ebur/debitum/ui/MainActivity.java
@@ -1,7 +1,6 @@
 
 package org.ebur.debitum.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -9,17 +8,30 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavArgument;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import org.ebur.debitum.R;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class MainActivity extends AppCompatActivity {
 
-    private NavController navController;
+    private final Set<Integer> DESTINATIONS_WITH_FAB = Stream.of(R.id.transactionListFragment, R.id.personSumListFragment)
+            .collect(Collectors.toCollection(HashSet::new));
+
+    private NavController nav;
     private Toolbar toolbar;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,24 +41,29 @@ public class MainActivity extends AppCompatActivity {
         // setup toolbar
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         assert navHostFragment != null;
-        navController = navHostFragment.getNavController();
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        nav = navHostFragment.getNavController();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(nav.getGraph()).build();
         toolbar = findViewById(R.id.toolbar);
-        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(toolbar, nav, appBarConfiguration);
         setSupportActionBar(toolbar);
 
         // setup fab
-        FloatingActionButton fab = findViewById(R.id.fab);
+        fab = findViewById(R.id.fab);
         fab.setOnClickListener(view -> {
-            Intent intent = new Intent(this, EditTransactionFragment.class);
-            startActivity(intent);
+            nav.navigate(R.id.editTransactionFragment);
+        });
+
+        // control FAB visibility
+        nav.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if(DESTINATIONS_WITH_FAB.contains(destination.getId())) fab.show();
+            else fab.hide();
         });
     }
 
     // TODO move to PersonSumList
     // TODO use safe args https://developer.android.com/guide/navigation/navigation-getting-started#ensure_type-safety_by_using_safe_args
     public void onSearchTransactionAction(MenuItem item) {
-        navController.navigate(R.id.action_personSumListFragment_to_transactionListFragment);
+        nav.navigate(R.id.action_personSumListFragment_to_transactionListFragment);
     }
 
     public void setToolbarTitle(int titleResId) {
