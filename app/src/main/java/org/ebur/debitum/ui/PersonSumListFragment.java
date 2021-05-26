c319fa1b76a2a4c703691a77fccd1a59d69f82c7
Marmo <Marmo@users.noreply.github.com>
2021-05-26 13:04:19 +0200

    PersonSumListFragment.java

--- app/src/main/java/org/ebur/debitum/ui/PersonSumListFragment.java
+++ app/src/main/java/org/ebur/debitum/ui/PersonSumListFragment.java
@@ -3,87 +3,43 @@ package org.ebur.debitum.ui;
 import android.graphics.drawable.InsetDrawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.ActionMode;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.res.ResourcesCompat;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
-import androidx.recyclerview.selection.ItemKeyProvider;
-import androidx.recyclerview.selection.MutableSelection;
-import androidx.recyclerview.selection.SelectionPredicates;
-import androidx.recyclerview.selection.SelectionTracker;
-import androidx.recyclerview.selection.StorageStrategy;
+import androidx.recyclerview.selection.Selection;
 import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.transition.MaterialFadeThrough;
 
 import org.ebur.debitum.R;
 import org.ebur.debitum.database.Person;
 import org.ebur.debitum.database.PersonWithTransactions;
-import org.ebur.debitum.database.Transaction;
 import org.ebur.debitum.viewModel.PersonSumListViewModel;
 
 import java.util.concurrent.ExecutionException;
 
-public class PersonSumListFragment extends Fragment {
+public class PersonSumListFragment extends AbstractBaseListFragment <
+        PersonSumListViewModel,
+        PersonSumListViewHolder,
+        PersonWithTransactions> {
 
     private static final String TAG = "PersonSumListFragment";
 
-    private PersonSumListViewModel viewModel;
-    private RecyclerView recyclerView;
-    private PersonSumListAdapter adapter;
-    private SelectionTracker<Long> selectionTracker = null;
-    private View emptyView;
-
-    private ActionMode actionMode;
-
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // Transitions
-        setEnterTransition(new MaterialFadeThrough().setDuration(400));
-        setExitTransition(new MaterialFadeThrough().setDuration(400));
+    int getLayout() {
+        return R.layout.fragment_person_sum_list;
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container,
-                             Bundle savedInstanceState) {
-        viewModel = new ViewModelProvider(requireActivity()).get(PersonSumListViewModel.class);
-
-        View root = inflater.inflate(R.layout.fragment_person_sum_list, container, false);
-
-        emptyView = root.findViewById(R.id.emptyDbView);
-
-        setupTotalHeader(root);
-        setupRecyclerView(root);
-        buildSelectionTracker();
-        subscribeToViewModel();
-        setHasOptionsMenu(true);
-        return root;
-    }
-
-    private void setupRecyclerView(View root) {
-        recyclerView = root.findViewById(R.id.recyclerview);
-        adapter = new PersonSumListAdapter(new PersonSumListAdapter.PersonSumDiff());
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(requireActivity()));
+    protected void setupRecyclerView(View root) {
+        super.setupRecyclerView(root);
         DividerItemDecoration decoration = new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL);
         InsetDrawable divider = (InsetDrawable) ResourcesCompat.getDrawable(getResources(), R.drawable.divider_inset_start, null);
         assert divider!=null;
@@ -92,7 +48,8 @@ public class PersonSumListFragment extends Fragment {
         recyclerView.addItemDecoration(decoration);
     }
 
-    private void subscribeToViewModel() {
+    @Override
+    protected void subscribeToViewModel() {
         viewModel.getPersonsWithTransactions().observe(getViewLifecycleOwner(), pwtList -> {
             updateTotalHeader(
                     PersonWithTransactions.getSum(pwtList)
@@ -108,118 +65,6 @@ public class PersonSumListFragment extends Fragment {
         });
     }
 
-    private void setupTotalHeader(View root) {
-        TextView descView = root.findViewById(R.id.header_description);
-        descView.setVisibility(View.INVISIBLE);
-    }
-
-    protected void updateTotalHeader(int totalMoney) {
-        TextView totalView = requireView().findViewById(R.id.header_total);
-        totalView.setText(Transaction.formatMonetaryAmount(totalMoney));
-        int totalColor = totalMoney>0 ? R.color.owe_green : R.color.lent_red;
-        totalView.setTextColor(totalView.getResources().getColor(totalColor, null));
-    }
-
-    private void buildSelectionTracker() {
-        selectionTracker = new SelectionTracker.Builder<>(
-                "personSumListSelection",
-                recyclerView,
-                new ItemKeyProvider<Long>(ItemKeyProvider.SCOPE_MAPPED) {
-                    @Override
-                    public Long getKey(int position) {
-                        return adapter.getItemId(position);
-                    }
-
-                    @Override
-                    public int getPosition(@NonNull Long key) {
-                        RecyclerView.ViewHolder viewHolder = recyclerView.findViewHolderForItemId(key);
-                        return viewHolder == null ? RecyclerView.NO_POSITION : viewHolder.getLayoutPosition();
-                    }
-                },
-                new ListItemDetailsLookup(recyclerView),
-                StorageStrategy.createLongStorage())
-                .withSelectionPredicate(SelectionPredicates.createSelectAnything())
-                .build();
-
-        // start action mode & change visible menu items depending on item selection
-        this.selectionTracker.addObserver(new SelectionTracker.SelectionObserver<Long>() {
-            /*@Override
-            protected void onSelectionCleared() {
-                actionMode.finish();
-            }*/
-
-            @Override
-            public void onSelectionChanged() {
-                if(actionMode == null) {
-                    actionMode = requireActivity().startActionMode(actionModeCallback);
-                } else if(!selectionTracker.hasSelection()) {
-                    actionMode.finish();
-                } else {
-                    actionMode.invalidate(); // refresh visible menu items
-                }
-            }
-        });
-        adapter.setSelectionTracker(this.selectionTracker);
-    }
-
-    // ----------------------
-    // Contextual action mode
-    // https://developer.android.com/guide/topics/ui/menus#CAB
-    // ----------------------
-
-    private final ActionMode.Callback actionModeCallback = new ActionMode.Callback() {
-        @Override
-        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-            MenuInflater inflater = mode.getMenuInflater();
-            inflater.inflate(R.menu.menu_person_sum_list_action_mode, menu);
-            actionMode = mode;
-            return true;
-        }
-
-        @Override
-        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-            int nRowsSelected = selectionTracker.getSelection().size();
-            // only show edit transaction menu item if exactly one transaction is selected
-            menu.findItem(R.id.miEditPerson).setVisible(nRowsSelected == 1);
-            // only show delete transaction menu item if one or more items are selected
-            menu.findItem(R.id.miDeletePerson).setVisible(nRowsSelected >= 1);
-            CharSequence title = getResources().getQuantityString(R.plurals.actionmode_selected, nRowsSelected, nRowsSelected);
-            mode.setTitle(title);
-            return true;
-        }
-
-        @Override
-        public boolean onActionItemClicked(ActionMode mode, MenuItem menuItem) {
-            int id = menuItem.getItemId();
-            if(id==R.id.miEditPerson) {
-                // get selected idPerson (note: PersonSumListAdapter.getItemId returns the
-                // item's person id as the unique row id, so we can use that here
-                int selectedId = selectionTracker.getSelection().iterator().next().intValue();
-                selectionTracker.clearSelection();
-                onEditPersonAction(selectedId);
-                mode.finish();
-                return true;
-            } else if(id==R.id.miDeletePerson) {
-                // make copy of selection so we have a constant list of selected items, even during
-                // iteratively deleting items
-                MutableSelection<Long> selectionCopy = new MutableSelection<>();
-                selectionTracker.copySelection(selectionCopy);
-                selectionTracker.clearSelection();
-                onDeletePersonAction(selectionCopy);
-                mode.finish();
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        @Override
-        public void onDestroyActionMode(ActionMode mode) {
-            selectionTracker.clearSelection();
-            actionMode = null;
-        }
-    };
-
     // ---------------------------
     // Toolbar Menu event handling
     // ---------------------------
@@ -233,14 +78,17 @@ public class PersonSumListFragment extends Fragment {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if(id==R.id.miAddPerson) {
-            onAddPersonAction();
+            addPerson();
             return true;
         } else {
             return super.onOptionsItemSelected(item);
         }
     }
 
-    public void onEditPersonAction(int idPerson) {
+    protected void onActionModeEdit(int idPerson) {
+        editPerson(idPerson);
+    }
+    public void editPerson(int idPerson) {
         Person selectedPerson;
         try {
             selectedPerson = viewModel.getPersonById(idPerson);
@@ -254,7 +102,10 @@ public class PersonSumListFragment extends Fragment {
         NavHostFragment.findNavController(this).navigate(R.id.action_editPerson, args);
     }
 
-    public void onDeletePersonAction(MutableSelection<Long> selection) {
+    protected void onActionModeDelete(Selection<Long> selection) {
+        deletePersons(selection);
+    }
+    public void deletePersons(Selection<Long> selection) {
         int deleteCount = selection.size();
 
         // ask for confirmation
@@ -279,7 +130,7 @@ public class PersonSumListFragment extends Fragment {
         dialog.show();
     }
 
-    public void onAddPersonAction() {
+    public void addPerson() {
         NavHostFragment.findNavController(this).navigate(R.id.action_addPerson);//, args);
     }
 }
